10608456a67c9406c294e099aa339fd309b47ca4
Fredrik Plumppu <59374117+trueplu@users.noreply.github.com>
2025-01-13 20:45:34 +0100

    update

---
+++ src/components/NavLogo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    type Props = { href: string; class?: string }
+    const { href = '', class: className = '' }: Props = $props()
+
+    function clearHash(event: MouseEvent) {
+        event.preventDefault()
+        window.history.pushState('', '', '/')
+        window.scrollTo(0, 0)
+    }
+</script>
