19326d348e54edf3e5ab6fedca48bd5d738ea576
Fredrik Plumppu <59374117+trueplu@users.noreply.github.com>
2025-02-02 23:35:52 +0100

    Update landing page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const SITE_NAME = 'Plumppu Musikproduktion'
 export const DEFAULT_LOCALE = 'sv-SE'
-export const SITE_DESCRIPTION = ''
+export const SITE_DESCRIPTION =
+    'Trumpet och musik till festliga sammanhang i Borås och Göteborg med omnejd.'
